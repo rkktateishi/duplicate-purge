b2007ac8ea55b8cd36175658be4a43b30d364fbd
Randall <rkktateishi@gmail.com>
2016-09-22 23:58:27 +0000

    Removed complex strip

--- static/js/base.js
+++ static/js/base.js
@@ -26,20 +26,6 @@ $(document).ready(function(){
     $('#short_length').html(Object.keys(unique).length);
     $('#short_info').show();
   });
-  $('#strip-complex').click(function(){
-    var starttime = new Date().getTime();
-    var unique = strip(long_list);
-    var endtime = new Date().getTime();
-    var time = endtime - starttime;
-    $('#long_list').hide();
-    for(uniq in unique){
-      $('#new_long').append('<li class="list-group-item col-xs-4">'+uniq+'</li>');
-    }
-    $('#new_long').show();
-    $('#long_time').html(time);
-    $('#long_length').html(Object.keys(unique).length);
-    $('#long_info').show();
-  });
   $('#strip-custom').click(function(){
     var custom_list = $('#my_list').val().split(',');
     var starttime = new Date().getTime();
